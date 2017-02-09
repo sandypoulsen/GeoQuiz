bb6136133673449a337643bd49181d2a6fdb8d1a
sandypoulsen <napo@itu.dk>
2017-02-09 11:59:15 +0100

    First commit

---
+++ app/src/main/java/dk/coconut/geoquiz/Question.java
@@ -0,0 +1,33 @@
+package dk.coconut.geoquiz;
+
+/**
+ * Created by alexanderpoulsen on 03/02/2017.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
